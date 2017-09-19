b573db488ae4bb6de0a4deaf5d18af19f927812b
Raul Perez <raulperod@gmail.com>
2017-09-18 17:26:58 -0700

    readme

--- Tarea 2/Diabetes/main.py
+++ Tarea 2/Diabetes/main.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     #Lectura simple de datos
     df = pd.read_csv( 'pima-indians-diabetes.data', names = ['emb', 'gl2h', 'pad', 'ept', 'is2h', 'imc', 'fpd', 'edad', 'class'] )
     # informacion
-    print(df.head(3))
+    print(df.head(10))
     print( df.info())
     print( df.describe() )
