ad787f9baf66eceb9a78d3f1213859b7c014d822
Raul Perez <raulperod@gmail.com>
2017-10-20 14:42:25 -0700

    Limpieza 1

--- 02-Limpieza-de-datos-1/Clase/04-interpolacion.py
+++ 02-Limpieza-de-datos-1/Clase/04-interpolacion.py
@@ -7,28 +7,38 @@ import os
 def lineal(df, fig, axes):
     df['ept'].plot(ax=axes[0])
     df['ept'].plot(ax=axes[0], grid=True, kind="bar")
-
+ 
     dfi = df['ept'].interpolate()
-
     dfi.plot(ax=axes[1])
     dfi.plot(ax=axes[1], grid=True, kind="bar", color="red")
     df['ept'].plot(ax=axes[1], grid=True, kind="bar")
-    print (df['ept'].interpolate())
-
-def cuadradas(df, fig, axes):
-    dfi = df['ept'].interpolate(method="quadratic")
-    dfi.plot(ax=axes[0])
-    dfi.plot(ax=axes[0], grid=True, kind="bar", color="red")
-    df['ept'].plot(ax=axes[0], grid=True, kind="bar")
-    print (df['ept'].interpolate())
-
-def cubicas(df, fig, axes):
+    print (dfi)
+    plt.plot(dfi)
+    plt.show()
     
+
+def cuadrada(df, fig, axes):
+    df['ept'].plot(ax=axes[0])
+    df['ept'].plot(ax=axes[0], grid=True, kind="bar")
+    dfi = df['ept'].interpolate(method="quadratic")
+    dfi.plot(ax=axes[1])
+    dfi.plot(ax=axes[1], grid=True, kind="bar", color="red")
+    df['ept'].plot(ax=axes[1], grid=True, kind="bar")
+    print (dfi)
+    plt.plot(dfi)
+    plt.show()
+
+def cubica(df, fig, axes):
+    df['ept'].plot(ax=axes[0])
+    df['ept'].plot(ax=axes[0], grid=True, kind="bar")
     dfi = df['ept'].interpolate(method="cubic")
     dfi.plot(ax=axes[1])
     dfi.plot(ax=axes[1], grid=True, kind="bar", color="red")
     df['ept'].plot(ax=axes[1], grid=True, kind="bar")
-    print (df['ept'].interpolate())
+    print (dfi)
+    plt.plot(dfi)
+    plt.show()
+
 
 if __name__ == '__main__':
     #me muevo a la carpeta data
@@ -40,5 +50,5 @@ if __name__ == '__main__':
     fig, axes = plt.subplots(nrows=2, ncols=1, sharex=True, sharey=True, figsize=(10,10))
     
     lineal(df, fig, axes)
-    cuadradas(df, fig, axes)
-    cubicas(df, fig, axes)
+    cuadrada(df, fig, axes)
+    cubica(df, fig, axes)
